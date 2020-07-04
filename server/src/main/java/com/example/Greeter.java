8997399d88027269f3086dab58e2c51598f34557
IngoSousa <ingomarlos@gmail.com>
2020-07-04 15:21:12 +0200

    Update Greeter.java

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,12 +11,12 @@ public class Greeter {
   public Greeter() {
 
   }
+  
   /**
-  @param this parm is bla bla
-  @return this returns blabla
-  **/
-
-  public String greet(final String someone) {
+  * @param this parm is bla bla
+  * @return this returns blabla
+  */
+  public final String greet(final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
